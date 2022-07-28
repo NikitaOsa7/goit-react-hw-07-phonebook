e328bfc6c5e0333931d8228019176c3160d06b59
Nikita Osadchyi <nikitaosadchyi7@gmail.com>
2022-07-28 16:56:55 +0300

    fixed

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,78 +1,69 @@
 import { useState } from 'react';
-import s from './ContactForm.module.css';
-import { useAddContactMutation, useGetContactsQuery } from '../../redux/contacts/contactsApi'
-import { nanoid } from 'nanoid';
+import {
+  useAddContactMutation,
+  useGetContactsQuery,
+} from '../../redux/services';
 
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import s from './ContactForm.module.css';
 
 export default function ContactForm() {
-    const [name, setName] = useState('');
-    const [number, setNumber] = useState('');
+  const [params, setParams] = useState({ name: '', phone: '' });
 
-    const { data } = useGetContactsQuery();
-    const [addCotnacts] = useAddContactMutation();
+  const [addContact] = useAddContactMutation();
+  const { data: contacts } = useGetContactsQuery();
 
+  const handleChange = e => {
+    const { name, value } = e.currentTarget;
+    setParams({ ...params, [name]: value });
+  };
 
-    const handleSumbit = e => {
-        e.preventDefault();
-        data.some(contact => contact.name === name)
-            ? alert(`${name} is already in contacts`)
-            : addCotnacts({
-                id: nanoid(),
-                name: name,
-                number: number,
-            });
-        
-        setName('');
-        setNumber('');
-    };
+  function handleSubmit(e) {
+    e.preventDefault();
+    contacts.some(contact => contact.name === params.name)
+      ? Notify.failure(`Contact ${params.name} already exists`)
+      : newContact();
+  }
 
-    const handleChange = evt => {
-        const { name, value } = evt.target;
-        switch (name) {
-            case 'name':
-                setName(value);
-                break;
-            case 'number':
-                setNumber(value);
-                break;
-            
-            default:
-                break;
-        }
-    };
+  const newContact = () => {
+    addContact(params);
+    Notify.info(`Contact ${params.name} added to phone book`);
+    reset();
+  };
 
-    return (
-        <form className={s.form} onSubmit={handleSumbit}>
-            <label className={s.label}>
-                Name
-                <input
-                    className={s.input}
-                    type="text"
-                    name="name"
-                    value={name}
-                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                    onChange={handleChange}
-                    required
-                />
-            </label>
-            <label className={s.label}>
-                Number
-                <input
-                    className={s.input}
-                    type="tel"
-                    name="number"
-                    value={number}
-                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                    onChange={handleChange}
-                    required
-                />
-            </label>
-            <button className={s.button} type='submit'>
-                Add contact
-            </button>
-        </form>
-    );
+  const reset = () => {
+    setParams({ name: '', phone: '' });
+  };
 
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name
+        <input
+          type="text"
+          name="name"
+          value={params.name}
+          onChange={handleChange}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </label>
+      <label>
+        Phone number
+        <input
+          type="tel"
+          name="phone"
+          value={params.phone}
+          onChange={handleChange}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </label>
+      <button type="submit" className={s.submitButton}>
+        Add contact
+      </button>
+    </form>
+  );
 }
